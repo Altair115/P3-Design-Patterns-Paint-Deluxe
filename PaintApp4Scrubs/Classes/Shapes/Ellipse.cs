bc15a51566a4f7b492349027fa35720f89ce5ede
Kevin <kevin.celinski@outlook.com>
2020-03-06 14:43:32 +0100

    :fire: Indent Fix

--- PaintApp4Scrubs/Classes/Shapes/Ellipse.cs
+++ PaintApp4Scrubs/Classes/Shapes/Ellipse.cs
@@ -1,16 +1,20 @@
 ﻿using System.Windows;
 using System.Windows.Media;
 
-namespace PaintApp4Scrubs.Classes.Shapes {
-  public class Ellipse : GodShape {
-    protected override Geometry DefiningGeometry {
-      get {
-        Point ellipseCenter = new Point(0.0d, 0.0d);
-        var ellipse =
-            new EllipseGeometry(ellipseCenter, ActualWidth, ActualHeight);
+namespace PaintApp4Scrubs.Classes.Shapes
+{
+    public class Ellipse : GodShape
+    {
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                Point ellipseCenter = new Point(0.0d, 0.0d);
+                var ellipse =
+                    new EllipseGeometry(ellipseCenter, ActualWidth, ActualHeight);
 
-        return ellipse;
-      }
+                return ellipse;
+            }
+        }
     }
-  }
 }
