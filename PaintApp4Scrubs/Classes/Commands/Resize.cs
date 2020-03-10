cd514515acf09f96fa1c795be25bd4f73f8bd789
Restyled.io <commits@restyled.io>
2020-03-10 11:21:09 +0000

    Restyled by clang-format

--- PaintApp4Scrubs/Classes/Commands/Resize.cs
+++ PaintApp4Scrubs/Classes/Commands/Resize.cs
@@ -6,21 +6,14 @@ using PaintApp4Scrubs.Classes.Shapes;
 using PaintApp4Scrubs.Classes.Commands;
 using PaintApp4Scrubs.Interfaces;
 
-namespace PaintApp4Scrubs.Classes
-{
-class Resize : ICommand
-{
+namespace PaintApp4Scrubs.Classes {
+  class Resize : ICommand {
     private GodShape shape;
     private Vector distance;
-    public Resize(GodShape _shape, Vector _distance)
-    {
-        shape = _shape;
-        distance = _distance;
-
+    public Resize(GodShape _shape, Vector _distance) {
+      shape = _shape;
+      distance = _distance;
     }
-    public void Execute()
-    {
-        shape.Rezise(distance);
-    }
-}
+    public void Execute() { shape.Rezise(distance); }
+  }
 }
--- PaintApp4Scrubs/Classes/Shapes/GodShape.cs
+++ PaintApp4Scrubs/Classes/Shapes/GodShape.cs
@@ -1,27 +1,16 @@
 ﻿using System.Windows;
 using System.Windows.Shapes;
 
-namespace PaintApp4Scrubs.Classes.Shapes
-{
-public abstract class GodShape : Shape
-{
-    public void Draw()
-    {
-        MainWindow.AppWindow.PutOnScreen(this);
-    }
-    public void Remove()
-    {
-        MainWindow.AppWindow.RemoveShape(this);
-    }
+namespace PaintApp4Scrubs.Classes.Shapes {
+  public abstract class GodShape : Shape {
+    public void Draw() { MainWindow.AppWindow.PutOnScreen(this); }
+    public void Remove() { MainWindow.AppWindow.RemoveShape(this); }
 
-    public void Rezise(Vector distance)
-    {
-        if (this.Width > distance.X &&
-                this.Height > distance.Y)
-        {
-            this.Width -= distance.X;
-            this.Height -= distance.Y;
-        }
+    public void Rezise(Vector distance) {
+      if (this.Width > distance.X && this.Height > distance.Y) {
+        this.Width -= distance.X;
+        this.Height -= distance.Y;
+      }
     }
-}
+  }
 }
