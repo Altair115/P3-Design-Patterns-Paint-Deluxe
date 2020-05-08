cab9a0b288722cedb5aeb316ab55009de6def740
Kevin <kevin.celinski@outlook.com>
2020-05-08 11:37:22 +0200

    :sparkles: Advanced Math Achieved

--- PaintApp4Scrubs/Classes/Commands/Move.cs
+++ PaintApp4Scrubs/Classes/Commands/Move.cs
@@ -18,6 +18,7 @@ namespace PaintApp4Scrubs.Classes.Commands
         private readonly IComponent _component;
         private readonly Vector _newPosition;
         private readonly Vector _oldPosition;
+        private readonly Vector _positionResult;
         private readonly VisitorMove _visitorMove;
 
         /// <summary>
@@ -25,11 +26,13 @@ namespace PaintApp4Scrubs.Classes.Commands
         /// </summary>
         /// <param name="component">the component that needs to be moved</param>
         /// <param name="newPosition">the new position where the component needs to go</param>
-        public Move(IComponent component, Vector newPosition, Vector startPoint)
+        /// <param name="oldPosition">the old position of the component</param>
+        public Move(IComponent component, Vector newPosition, Vector oldPosition)
         {
             _component = component;
-            _newPosition = newPosition;            
-            _visitorMove = new VisitorMove(_newPosition);
+            _newPosition = newPosition;
+            _oldPosition = oldPosition;
+            _positionResult = _oldPosition + -1 * _newPosition;
         }
 
         /// <summary>
@@ -37,15 +40,14 @@ namespace PaintApp4Scrubs.Classes.Commands
         /// </summary>
         public void Execute()
         {
-            _component.Accept(_visitorMove);
+            _component.Accept(new VisitorMove(Vector.Subtract(_oldPosition, _positionResult)));
         }
         /// <summary>
         /// executes the undo version of the command 
         /// </summary>
         public void UnExecute()
         {
-            _visitorMove.NewPosition = _visitorMove.OldPosition;
-            _component.Accept(_visitorMove);
+            _component.Accept(new VisitorMove(Vector.Add(_newPosition, _positionResult)));
         }
     }
 }
