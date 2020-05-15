559067b3c5fcbfe7a6ff9e8f984c30978a218dac
Kevin <kevin.celinski@outlook.com>
2020-05-15 14:26:12 +0200

    :alembic:

--- PaintApp4Scrubs/Classes/Ornament.cs
+++ PaintApp4Scrubs/Classes/Ornament.cs
@@ -1,10 +1,14 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace PaintApp4Scrubs.Classes
 {
     public class Ornament : Decorator
     {
+        public string Name;
+        public Vector OrnamentPos;
+
         protected override Geometry DefiningGeometry { get; }
 
         public override void Decorate()
@@ -12,7 +16,7 @@ namespace PaintApp4Scrubs.Classes
             base.Decorate();
             //do stuff
             TextBlock textBlock = new TextBlock();
-            textBlock.Text = this.ToString();
+            textBlock.Text = Name.ToString();
             Canvas.SetTop(textBlock, 100);
             Canvas.SetLeft(textBlock, 100);
             MainWindow.AppWindow.PutOnScreen(textBlock);
