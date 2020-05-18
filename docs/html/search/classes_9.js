639a220a08d2cdc7104506d394e4b79d09a125ca
Kevin <kevin.celinski@outlook.com>
2020-05-18 16:02:14 +0200

    :art:

---
+++ docs/html/search/classes_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ornament_73',['Ornament',['../class_paint_app4_scrubs_1_1_classes_1_1_ornament.html',1,'PaintApp4Scrubs::Classes']]]
+];
---
+++ docs/html/search/functions_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['putonscreen_101',['PutOnScreen',['../class_paint_app4_scrubs_1_1_main_window.html#a26f3df0af923c19c2e9413a39f0320aa',1,'PaintApp4Scrubs::MainWindow']]]
+];
