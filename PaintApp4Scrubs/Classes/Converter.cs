a2e478dd25fd55187979f511f778fa2502385acd
Kevin <kevin.celinski@outlook.com>
2020-05-25 11:30:34 +0200

    :pencil: Cleaning Tag Team 2: Electric Bugaloo

--- PaintApp4Scrubs/Classes/Converter.cs
+++ PaintApp4Scrubs/Classes/Converter.cs
@@ -2,12 +2,25 @@
 
 namespace PaintApp4Scrubs.Classes
 {
+    /// <summary>
+    /// A Simple Vector 2 Point or Point to Vector Converter
+    /// </summary>
     public class Converter
     {
+        /// <summary>
+        /// Converts a Point to a Vector
+        /// </summary>
+        /// <param name="point">the point to be converted</param>
+        /// <returns>A Vector</returns>
         public static Vector ToVector(Point point)
         {
             return new Vector(point.X, point.Y);
         }
+        /// <summary>
+        /// Convert a Vector to a Point
+        /// </summary>
+        /// <param name="vector">the vector to be converted</param>
+        /// <returns>A Point</returns>
         public static Point ToPoint(Vector vector)
         {
             return new Point(vector.X, vector.Y);
--- PaintApp4Scrubs/Classes/ThePrinter.cs
+++ PaintApp4Scrubs/Classes/ThePrinter.cs
@@ -1,5 +1,8 @@
 ﻿namespace PaintApp4Scrubs.Classes
 {
+    /// <summary>
+    /// A Simple Printer Singleton
+    /// </summary>
     public class ThePrinter
     {
         public static ThePrinter InstancePrinter = new ThePrinter();
@@ -11,12 +14,19 @@
             return InstancePrinter;
         }
 
+        /// <summary>
+        /// Print the String to File
+        /// </summary>
+        /// <param name="text">the String to be printed</param>
         public void PrintToFile(string text)
         {
             using System.IO.StreamWriter file =
                 new System.IO.StreamWriter(@"..\..\..\..\comptest.txt", true);
             file.WriteLine(text);
         }
+        /// <summary>
+        /// Clear the File
+        /// </summary>
         public void ClearFile()
         {
             System.IO.File.WriteAllText(@"..\..\..\..\comptest.txt", "");
